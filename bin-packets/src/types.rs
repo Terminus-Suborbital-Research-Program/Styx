0f592ab11349275c1ec03e7ffa5a36cc81dabbcb
Michael Angeles <michael.g.angeles21@gmail.com>
2025-04-26 18:59:16 -0500

    adding static buffer and sensor types

--- bin-packets/src/types.rs
+++ bin-packets/src/types.rs
@@ -1,6 +1,7 @@
 use bincode::{Decode, Encode};
 use defmt::Format;
 use serde::{Deserialize, Serialize};
+use fugit::Instant;
 
 /// Used to represent unix timestamp in milliseconds
 #[derive(
@@ -178,5 +179,79 @@ mod timestamp_tests {
         let timestamp1 = UnixTimestampMillis::new(1000);
         let timestamp2 = UnixTimestampMillis::new(2000);
         let _ = (timestamp1 - timestamp2).unwrap();
+    }
+}
+
+
+const BUFFER_LENGTH: usize = 10;
+// Sensor Data Structs
+#[derive(Default,Debug,Clone,Copy,Encode,Decode,Serialize,Deserialize)]
+pub struct PowerData{
+    time_stamp: u64,
+    power: (i8, u32),
+}
+#[derive(Default,Debug,Clone,Copy,Encode,Decode,Serialize,Deserialize)]
+pub struct CurrentData{
+    time_stamp: u64,
+    current: (i8, u32),
+}
+#[derive(Default,Debug,Clone,Copy,Encode,Decode,Serialize,Deserialize)]
+pub struct VoltageData{
+    time_stamp: u64,
+    voltage: (u8, u32),
+}
+
+#[derive(Default,Debug,Clone,Copy,Encode,Decode,Serialize,Deserialize)]
+pub struct MasterData{
+    power_1: StaticBuffer<PowerData>,
+    power_2: StaticBuffer<PowerData>,
+    power_3: StaticBuffer<PowerData>,
+    current_1: StaticBuffer<PowerData>,
+    current_2: StaticBuffer<PowerData>,
+    current_3: StaticBuffer<PowerData>,
+    voltage_1: StaticBuffer<PowerData>,
+    voltage_2: StaticBuffer<PowerData>,
+    voltage_3: StaticBuffer<PowerData>,
+}
+
+pub struct StaticBuffer<T> {
+    buffer: [T; BUFFER_LENGTH],
+    index: usize,
+}
+impl StaticBuffer<T> {
+    pub fn new() -> Self {
+        Self {
+            buffer: [T::default(); BUFFER_LENGTH],
+            index: 0,
+        }
+    }
+    pub fn push(&mut self, data: T) {
+        if self.index == BUFFER_LENGTH - 1 {
+            // Shift all elements to the left, removing the first item
+            for i in 0..BUFFER_LENGTH - 1 {
+                self.buffer[i] = self.buffer[i + 1];
+            }
+            self.index = BUFFER_LENGTH - 1;
+            // Add the new data to the last position
+            self.buffer[self.index] = data;
+        } else {
+            // Add the new data at the current index
+            self.buffer[self.index] = data;
+            self.index += 1;
+        }
+    }
+    /// Removes and returns the last element in the buffer if it exists.
+    /// 
+    /// If the buffer is empty (index is 0), it returns `None`.
+    /// Otherwise, it decreases the index by 1 and returns the element
+    /// at the previous index.
+    pub fn pop(&mut self) -> Option<T> {
+        if self.index == 0 {
+            None
+        } else {
+            let data = self.buffer[self.index - 1];
+            self.index -= 1;
+            Some(data)
+        }
     }
 }
