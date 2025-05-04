645fa4c7bbeff5c0fcba14bbd9312402c668d3bd
Lucas Thelen <thelenlucas028@gmail.com>
2025-05-04 16:24:24 -0500

    RBF pin work

--- jupiter-fsw/src/rbf.rs
+++ jupiter-fsw/src/rbf.rs
@@ -1,17 +1,32 @@
+use log::warn;
+
 use crate::gpio::read::ReadPin;
 
 pub struct RbfPin {
     pin: ReadPin,
+    inserted_at_boot: bool,
 }
 
 impl From<ReadPin> for RbfPin {
     fn from(pin: ReadPin) -> Self {
-        RbfPin { pin }
+        let boot = match pin.read() {
+            Ok(s) => s,
+            Err(e) => {
+                warn!("Failed to read RBF pin: {:?}", e);
+                true
+            }
+        };
+
+        RbfPin {
+            pin,
+            inserted_at_boot: boot,
+        }
     }
 }
 
 impl RbfPin {
-    pub fn is_inserted(&self) -> bool {
-        self.pin.read().unwrap()
+    pub fn inhibited(&self) -> bool {
+        // Inserted at boot
+        self.inserted_at_boot
     }
 }
