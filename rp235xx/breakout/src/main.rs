5aca304516aa3a826c8b0757a38d063fa5aeb0c2
Michael Angeles <michael.g.angeles21@gmail.com>
2025-05-28 23:53:19 -0500

    adding breakout target for software integration

---
+++ rp235xx/breakout/src/main.rs
@@ -0,0 +1,151 @@
+// Specifies that the standard library is not used
+#![no_std]
+#![no_main]
+
+// Our Modules
+pub mod actuators;
+pub mod device_constants;
+pub mod peripherals;
+pub mod phases;
+pub mod sensors;
+pub mod startup;
+pub mod tasks;
+pub mod utilities;
+
+use defmt_rtt as _; // global logger
+
+use crate::tasks::*;
+use core::mem::MaybeUninit;
+
+// Sensors
+use bme280_rs::AsyncBme280;
+use bmi323::AsyncBMI323;
+use ina260_terminus::AsyncINA260;
+use bmm350::AsyncBMM350;
+
+// Busses
+use rtic_sync::arbiter::i2c::ArbiterDevice;
+
+/// Lets us know when we panic
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    defmt::error!("Panic: {}", info);
+    // Better panic
+    hal::halt();
+}
+
+// HAL Access
+#[cfg(feature = "rp2350")]
+use rp235x_hal as hal;
+
+// Monotonics
+#[cfg(feature = "rp2350")]
+use rtic_monotonics::rp235x::prelude::*;
+#[cfg(feature = "rp2350")]
+rp235x_timer_monotonic!(Mono);
+
+/// Tell the Boot ROM about our application
+#[link_section = ".start_block"]
+#[used]
+#[cfg(feature = "rp2350")]
+pub static IMAGE_DEF: rp235x_hal::block::ImageDef = rp235x_hal::block::ImageDef::secure_exe();
+
+#[rtic::app(
+    device = hal::pac,
+    dispatchers = [PIO2_IRQ_0, PIO2_IRQ_1, DMA_IRQ_0],
+    peripherals = true,
+)]
+mod app {
+    use crate::device_constants::{
+        servos::{FlapServo, RelayServo},
+        AvionicsI2cBus, DownlinkBuffer, IcarusRadio, MotorI2cBus,
+    };
+
+    use super::*;
+
+    use bin_packets::{phases::IcarusPhase, time::Timestamp};
+
+    use hal::gpio::{self, FunctionSio, PullNone, SioOutput};
+    use rp235x_hal::uart::UartPeripheral;
+    pub const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+    use rtic_sync::{arbiter::Arbiter, signal::Signal};
+
+    pub type UART0Bus = UartPeripheral<
+        rp235x_hal::uart::Enabled,
+        rp235x_hal::pac::UART0,
+        (
+            gpio::Pin<gpio::bank0::Gpio0, gpio::FunctionUart, gpio::PullDown>,
+            gpio::Pin<gpio::bank0::Gpio1, gpio::FunctionUart, gpio::PullDown>,
+        ),
+    >;
+
+    // pub static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+    #[shared]
+    pub struct Shared {
+        pub data: DownlinkBuffer,
+    }
+
+    #[local]
+    pub struct Local {
+        pub radio: IcarusRadio,
+        pub relay_servo: RelayServo,
+        pub flap_servo: FlapServo,
+        pub led: gpio::Pin<gpio::bank0::Gpio25, FunctionSio<SioOutput>, PullNone>,
+        pub bmm350: AsyncBMM350<ArbiterDevice<'static, AvionicsI2cBus>, Mono>,
+        pub bmi323: AsyncBMI323<ArbiterDevice<'static, AvionicsI2cBus>, Mono>,
+        pub bme280: AsyncBme280<ArbiterDevice<'static, AvionicsI2cBus>, Mono>,
+        pub ina260_1: AsyncINA260<ArbiterDevice<'static, MotorI2cBus>, Mono>,
+        pub ina260_2: AsyncINA260<ArbiterDevice<'static, MotorI2cBus>, Mono>,
+        pub ina260_3: AsyncINA260<ArbiterDevice<'static, MotorI2cBus>, Mono>,
+    }
+
+    #[init(
+        local=[
+            // Task local initialized resources are static Here we use MaybeUninit to allow for initialization in init()
+            // This enables its usage in driver initialization
+            i2c_avionics_bus: MaybeUninit<Arbiter<AvionicsI2cBus>> = MaybeUninit::uninit(),
+            i2c_motor_bus: MaybeUninit<Arbiter<MotorI2cBus>> = MaybeUninit::uninit(),
+            esc_state_signal: MaybeUninit<Signal<IcarusPhase>> = MaybeUninit::uninit(),
+        ]
+    )]
+    fn init(ctx: init::Context) -> (Shared, Local) {
+        startup::startup(ctx)
+    }
+
+    extern "Rust" {
+        // Heartbeats the main led
+        #[task(local = [led], shared = [data], priority = 1)]
+        async fn heartbeat(ctx: heartbeat::Context);
+
+        // Takes care of incoming packets
+        #[task(local = [radio], shared = [data], priority = 1)]
+        async fn radio_send(mut ctx: radio_send::Context);
+
+        #[task(priority = 3, local=[flap_servo, relay_servo])]
+        async fn mode_sequencer(&mut ctx: mode_sequencer::Context);
+
+        // Handles INA sensors
+        #[task(priority = 2, shared = [data], local=[ina260_1, ina260_2, ina260_3])]
+        async fn ina_sample(&mut ctx: ina_sample::Context, i2c: &'static Arbiter<MotorI2cBus>);
+
+        #[task(local = [bme280, bmi323, bmm350], priority = 2)]
+        async fn sample_sensors(
+            mut ctx: sample_sensors::Context,
+            avionics_i2c: &'static Arbiter<AvionicsI2cBus>,
+        );
+
+        #[task(priority = 2)]
+        async fn inertial_nav(mut ctx: inertial_nav::Context);
+    }
+
+    /// Returns the current time in nanoseconds since power-on
+    pub fn epoch_ns() -> u64 {
+        Mono::now().duration_since_epoch().to_nanos()
+    }
+
+    /// Returns the current time as a timestamp
+    pub fn now_timestamp() -> Timestamp {
+        Timestamp::new(epoch_ns())
+    }
+}
---
+++ rp235xx/breakout/src/tasks.rs
@@ -0,0 +1,244 @@
+use bin_packets::device::PacketWriter;
+use bin_packets::devices::DeviceIdentifier;
+use bin_packets::packets::status::Status;
+use bin_packets::packets::ApplicationPacket;
+use bme280_rs::{Configuration, Oversampling, SensorMode};
+use defmt::{error, info};
+use embedded_hal::digital::StatefulOutputPin;
+use fugit::ExtU64;
+use futures::join;
+use heapless::Vec;
+use rtic::Mutex;
+use rtic_monotonics::Monotonic;
+use rtic_sync::arbiter::Arbiter;
+
+use crate::device_constants::AvionicsI2cBus;
+use crate::phases::{FlapServoStatus, Modes, RelayServoStatus};
+use crate::{app::*, device_constants::MotorI2cBus, Mono};
+
+pub async fn heartbeat(mut ctx: heartbeat::Context<'_>) {
+    let mut sequence_number = 0;
+    loop {
+        _ = ctx.local.led.toggle();
+
+        let status = Status::new(DeviceIdentifier::Icarus, now_timestamp(), sequence_number);
+
+        ctx.shared
+            .data
+            .lock(|data| data.push_back(status.into()).ok());
+
+        sequence_number = sequence_number.wrapping_add(1);
+
+        Mono::delay(300_u64.millis()).await;
+    }
+}
+
+pub async fn radio_send(mut ctx: radio_send::Context<'_>) {
+    let radio = ctx.local.radio;
+    loop {
+        if radio.waiting() > 0 {
+            while ctx.shared.data.lock(|data| data.len()) < 1 {
+                Mono::delay(100.millis()).await;
+            }
+        }
+
+        ctx.shared.data.lock(|data| {
+            while let Some(packet) = data.front() {
+                if radio.add(packet).is_ok() {
+                    let _ = data.pop_front();
+                }
+            }
+        });
+
+        if radio.waiting() > 0 {
+            let written = radio.write(32).unwrap_or(0) as u64;
+            Mono::delay((written / (9600 / 8)).secs()).await;
+            info!("Wrote {} bytes to the buffer", written);
+        }
+    }
+}
+
+use rp235x_pac::interrupt;
+#[interrupt]
+unsafe fn I2C0_IRQ() {
+    MotorI2cBus::on_interrupt();
+}
+
+use crate::phases::mode::{FLUTTER_COUNT, FLUTTER_START_TIME, SERVO_DISABLE_DELAY};
+pub async fn mode_sequencer(ctx: mode_sequencer::Context<'_>) {
+    let status = 0;
+    let iteration = 0;
+    let mut mode_start = Mono::now();
+
+    let mut flap_status = false;
+    let mut relay_status = false;
+    ctx.local.relay_servo.enable();
+    ctx.local.flap_servo.enable();
+    ctx.local.flap_servo.deg_0();
+    ctx.local.relay_servo.deg_0();
+    let mut relay_flutter_status = RelayServoStatus::Open;
+    let mut flap_flutter_status = FlapServoStatus::Open;
+    let mut flutter_count = 0;
+    loop {
+        if flap_status == false {
+            flap_status = Modes::open_flaps_sequence(mode_start, ctx.local.flap_servo).await;
+            relay_status =
+                Modes::relay_eject_servo_sequence(mode_start, ctx.local.relay_servo).await;
+        } else {
+            Mono::delay(FLUTTER_START_TIME.millis()).await;
+            if flutter_count < FLUTTER_COUNT {
+                mode_start = Mono::now();
+                flap_flutter_status = Modes::flap_flutter_sequence(
+                    mode_start,
+                    flap_flutter_status,
+                    ctx.local.flap_servo,
+                )
+                .await;
+                relay_flutter_status = Modes::relay_flutter_sequence(
+                    mode_start,
+                    relay_flutter_status,
+                    ctx.local.relay_servo,
+                )
+                .await;
+                flutter_count += 1;
+            } else {
+                ctx.local.flap_servo.deg_0();
+                ctx.local.relay_servo.deg_0();
+                Mono::delay(SERVO_DISABLE_DELAY.millis()).await;
+                ctx.local.flap_servo.disable();
+                ctx.local.relay_servo.disable();
+            }
+        }
+        Mono::delay(5_u64.millis()).await;
+    }
+}
+
+pub async fn ina_sample(mut ctx: ina_sample::Context<'_>, _i2c: &'static Arbiter<MotorI2cBus>) {
+    info!("Motor Driver Task Started");
+
+    if let Err(e) = ctx.local.ina260_1.init().await {
+        error!("Error initializing INA 1: {:?}", e);
+    }
+    Mono::delay(2_u64.millis()).await;
+
+    if let Err(e) = ctx.local.ina260_2.init().await {
+        error!("Error initializing INA 2: {:?}", e);
+    }
+    Mono::delay(2_u64.millis()).await;
+
+    if let Err(e) = ctx.local.ina260_3.init().await {
+        error!("Error initializing INA 1: {:?}", e);
+    }
+    Mono::delay(2_u64.millis()).await;
+
+    ctx.local.ina260_1.set_operating_mode(ina260_terminus::OperMode::SCBVC).await;
+    ctx.local.ina260_2.set_operating_mode(ina260_terminus::OperMode::SCBVC).await;
+    ctx.local.ina260_3.set_operating_mode(ina260_terminus::OperMode::SCBVC).await;
+
+    loop {
+        let items = join!(
+            ctx.local.ina260_1.voltage(),
+            // ctx.local.ina260_2.voltage(),
+            // ctx.local.ina260_3.voltage()
+        );
+        info!("Currents: {}", ctx.local.ina260_1.current().await); //, ctx.local.ina260_2.current().await, ctx.local.ina260_3.current().await);
+        info!("Powers: {}", ctx.local.ina260_1.power().await); //, ctx.local.ina260_2.power().await, ctx.local.ina260_3.power().await);
+        info!("Voltages: {}", items.0);
+        // let voltages = [
+        //     items.0.unwrap_or(f32::NAN),
+        //     items.1.unwrap_or(f32::NAN),
+        //     items.2.unwrap_or(f32::NAN),
+        // ];
+        // let packet = ApplicationPacket::VoltageData {
+        //     timestamp: epoch_ns(),
+        //     voltage: voltages,
+        // };
+
+        // ctx.shared.data.lock(|vec| vec.push_back(packet).ok());
+
+        Mono::delay(50.millis()).await;
+    }
+}
+
+pub async fn sample_sensors(
+    ctx: sample_sensors::Context<'_>,
+    _avionics_i2c: &'static Arbiter<AvionicsI2cBus>,
+) {
+    let bme_on = ctx.local.bme280.init().await;
+    match bme_on {
+        Ok(_) => {}
+        Err(i2c_error) => {
+            error!("BME Error: {}", i2c_error);
+        }
+    }
+    Mono::delay(10_u64.millis()).await;
+    ctx.local
+        .bme280
+        .set_sampling_configuration(
+            Configuration::default()
+                .with_temperature_oversampling(Oversampling::Oversample1)
+                .with_pressure_oversampling(Oversampling::Oversample1)
+                .with_humidity_oversampling(Oversampling::Oversample1)
+                .with_sensor_mode(SensorMode::Normal),
+        )
+        .await
+        .expect("Failed to configure BME280");
+
+    let bme_id = ctx.local.bme280.chip_id().await;
+    match bme_id {
+        Ok(id) => {
+            info!("BME280 ID: {}", id);
+        }
+        Err(i2c_error) => {
+            info!("I2CError: {}", i2c_error)
+        }
+    }
+
+    // TODO: (Remove me if no effect) Delaying preemptive to other processes just in case...
+    Mono::delay(10_u64.millis()).await;
+    let bmi323_init_result = ctx.local.bmi323.init().await;
+    match bmi323_init_result {
+        Ok(_) => {
+            info!("BMI Initialized");
+        }
+        Err(_) => {
+            error!("BMI Unininitialized");
+        }
+    }
+
+    Mono::delay(10_u64.millis()).await; // !TODO (Remove me if no effect) Delaying preemptive to other processes just in case...
+    let bmi323_init_result = ctx.local.bmi323.init().await;
+    match bmi323_init_result{
+        Ok(_)=>{
+            info!("BMI Initialized");
+        }
+        Err(_)=>{
+            error!("BMI Unininitialized");
+        }
+    }
+    Mono::delay(10_u64.millis()).await;
+    loop {
+        let sample_result = ctx.local.bme280.read_sample().await;
+        match sample_result {
+            Ok(sample) => {
+                let temperature = sample.temperature.unwrap();
+                let humidity = sample.humidity.unwrap();
+                let pressure = sample.pressure.unwrap();
+                info!("Sample: ┳ Temperature: {} C", temperature);
+                info!("        ┣ Humidity: {} %", humidity);
+                info!("        ┗ Pressure: {} hPa", pressure);
+            }
+            Err(i2c_error) => {
+                error!("I2C Error: {}", i2c_error)
+            }
+        }
+        Mono::delay(250_u64.millis()).await;
+    }
+}
+
+pub async fn inertial_nav(_ctx: inertial_nav::Context<'_>) {
+    loop {
+        // info!("Inertial Navigation");
+        Mono::delay(250_u64.millis()).await;
+    }
+}
