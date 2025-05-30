1a3bcb1b319c15f475516f9c7a9632cc349e742b
Lucas Thelen <thelenlucas028@gmail.com>
2025-05-29 22:08:59 -0500

    Added pulses

--- rp235xx/ejector/src/main.rs
+++ rp235xx/ejector/src/main.rs
@@ -46,7 +46,7 @@ mod app {
     use crate::actuators::servo::EjectorServo;
     use crate::device_constants::pins::CamMosfetPin;
     use crate::device_constants::{
-        EjectionDetectionPin, GreenLed, JupiterUart, OnboardLED, RedLed,
+        EjectionDetectionPin, GreenLed, JupiterUart, OnboardLED, RedLed, SAMPLE_COUNT,
     };
 
     use super::*;
@@ -57,6 +57,7 @@ mod app {
     use hal::gpio::{self};
 
     use heapless::Deque;
+    use rp235x_hal::adc::AdcFifo;
     use rp235x_hal::uart::UartPeripheral;
     pub const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
@@ -71,8 +72,9 @@ mod app {
 
     #[shared]
     pub struct Shared {
-        pub downlink_packets: Deque<ApplicationPacket, 16>,
+        pub downlink_packets: Deque<ApplicationPacket, 128>,
         pub radio: RadioInterface,
+        pub samples_buffer: [u16; SAMPLE_COUNT],
     }
 
     #[local]
@@ -84,9 +86,10 @@ mod app {
         pub ejection_pin: EjectionDetectionPin,
         pub downlink: JupiterUart,
         pub camera_mosfet: CamMosfetPin,
+        pub geiger_fifo: Option<AdcFifo<'static, u16>>,
     }
 
-    #[init]
+    #[init(local = [adc: Option<hal::Adc> = None])]
     fn init(ctx: init::Context) -> (Shared, Local) {
         startup::startup(ctx)
     }
@@ -111,6 +114,12 @@ mod app {
         // Updates the radio module on the serial interrupt
         #[task(binds = UART1_IRQ, shared = [radio])]
         fn uart_interrupt(mut ctx: uart_interrupt::Context);
+
+        #[task(binds = ADC_IRQ_FIFO, priority = 1, shared = [samples_buffer], local = [geiger_fifo, counter: usize = 1])]
+        fn adc_irq(mut ctx: adc_irq::Context);
+
+        #[task(priority = 2, shared = [samples_buffer, downlink_packets])]
+        async fn geiger_calculator(mut ctx: geiger_calculator::Context);
     }
 
     /// Returns the current time in nanoseconds since power-on
