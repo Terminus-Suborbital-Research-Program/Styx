666fa0414d4911ca4f1a29b63f2fbcd3b644fe20
Lucas Thelen <thelenlucas028@gmail.com>
2025-06-06 19:39:05 -0500

    Version Bump

--- jupiter-fsw/jupiter.nix
+++ jupiter-fsw/jupiter.nix
@@ -2,7 +2,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "jupiter-fsw";
-  version = "0.0.1";
+  version = "0.0.3";
 
   nativeBuildInputs = [ pkg-config libudev-zero ];
 
@@ -11,9 +11,9 @@ rustPlatform.buildRustPackage (finalAttrs: {
   src = fetchFromGitHub {
     owner = "Terminus-Suborbital-Research-Program";
     repo = "AMALTHEA";
-    rev = "v0.0.2";
+    rev = "v0.0.3";
     fetchSubmodules = true;
-    hash = "sha256-SA8plLtknepmkiWc2Ex0d6cnF8Gyf6P6Gt1GuUY8Xb4=";
+    hash = "";
   };
 
   cargoHash = "sha256-zOrvDUZ5gIlroafu7IntT6SmoBnpNg+k/vfVQw04TdI=";
