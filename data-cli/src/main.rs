a82f3019814d28e4f7b36cd5a04b2f32fd249311
supergoodname77 <epascuales@outlook.com>
2025-06-25 09:50:52 -0400

    Prototyping

--- data-cli/src/main.rs
+++ data-cli/src/main.rs
@@ -1,10 +1,12 @@
 use bin_packets::packets::ApplicationPacket;
 use bincode::{config::standard, decode_from_std_read};
 use clap::{Parser, Subcommand};
+use csv::Writer;
+use indexmap::IndexMap;
+
 
 use std::{
-    fs::{File, OpenOptions},
-    io::{BufReader, BufWriter, ErrorKind, Read, Write},
+    collections::HashMap, fs::{File, OpenOptions}, io::{BufReader, BufWriter, ErrorKind, Read, Write}
 };
 
 #[derive(Parser)]
@@ -72,8 +74,43 @@ impl Commands {
 
                     match data {
                         Ok(packet) => {
+
+                            // Temp
+                            let mut wrt = Writer::from_path("/home/supergoodname77/Desktop/Final Flash/AMALTHEA/data-cli/src/temp.csv").expect("Wrong path for csv");
+                            //
                             if stdout {
                                 println!("{packet:#?}");
+                                let value = serde_json::to_value(packet).unwrap();
+                                if let Some(headers_map) = Commands::parse_packet(&value) {
+                                    
+                                    ///
+                                    /// Need to name new csv file as the name of the struct.
+                                    /// If the name already exists, we can writing the headers and only write the values
+                                    /// of the current iteration.
+                                    /// Also needs some mechanism for when to start and stop some new iteration of a 
+                                    /// csv file for the same struct.
+                                    //
+
+                                    // headers_map.iter().map(|k,v| {
+                                    if 
+                                    headers_map.keys().next()
+                                    // })
+
+                                    // let mut headers_keys = headers_map.keys();
+                                    // println!("{headers_keys:#?}");
+                                    // // headers_map.values();
+                                    // wrt.write_record(headers_keys.clone()).expect("Ooops, failure writing headers");
+                                    // let mut output_buf = Vec::new();
+                                    // // println!("{headers_keys:#?}");
+
+                                    // for key in &mut headers_keys {
+                                    //     output_buf.push(&headers_map[key]);
+                                    //     // wrt.write_record(headers_keys)key
+                                    // }
+                                    // // println!("{output_buf:#?}");
+                                    // wrt.write_record(output_buf).expect("Ooops, failure writing values");
+                                }
+                                
                             }
                             if output {
                                 if let Some(ref mut file_writer) = writer {
@@ -103,6 +140,29 @@ impl Commands {
         if let Err(e) = writeln!(file_writer, "{packet:#?}") {
             eprintln!("Error appending to output file: {e}")
         }
+    }
+
+    fn parse_packet(mut packet: &serde_json::Value) -> Option<HashMap<String, String>> {
+
+        let mut result = None;
+
+        if let Some(field) = packet.as_object(){
+
+            let mut incomplete_map = HashMap::new();
+            for key in field.keys() {
+                if let Some(map) = Commands::parse_packet(&field[key]) {
+                    // Over here an insertion would have to be added if we also want to view the name
+                    // of sub_structs, but this might be unneccessary because it is counter intuitive to the
+                    // format of CSV files
+                    incomplete_map.extend(map);
+                } else {
+                    incomplete_map.insert(key.to_string(), field[key].to_string());
+                }
+            }
+            result = Some(incomplete_map);
+        }
+
+        result
     }
 }
 fn main() {
