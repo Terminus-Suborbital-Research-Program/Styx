71b7c1d53a31529738c496a4d6619e70fe100fab
supergoodname77 <epascuales@outlook.com>
2026-03-18 16:42:39 -0500

    Remove aether from binpackets

--- common/messages/bin-packets/src/data/adcs.rs
+++ common/messages/bin-packets/src/data/adcs.rs
@@ -2,22 +2,24 @@ use crate::time::{Timestamp};
 
 
 use bincode::{Decode, Encode};
-use serde::{Serialize, Deserialize};
+// use serde::{Serialize, Deserialize};
 
-#[cfg(feature = "std")]
-use aether::attitude::Quaternion;
 
-#[cfg(feature = "std")]
-use aether::reference_frame::{
-    Body,
-    ICRF,
-};
+// Currently quaternions and reference frames pull STD, so switching to tranposrting a raw vector
+// #[cfg(feature = "aether")]
+// use aether::attitude::Quaternion;
+
+// #[cfg(feature = "aether")]
+// use aether::reference_frame::{
+//     Body,
+//     ICRF,
+// };
 
 // #[derive(Debug, Clone, Copy, Encode, Decode, Format, Serialize, Deserialize)]
-#[cfg(feature = "std")]
+// #[cfg(feature = "aether")]
 #[derive(Debug, Clone, Copy, Encode, Decode)]
 pub struct AttitudeMetrics {
     pub timestamp: Timestamp,
-    pub quaternion: Quaternion<f32, ICRF<f32>,Body<f32>>,
+    pub quaternion: [f32; 4],
     pub signal_match: f32,
 }
