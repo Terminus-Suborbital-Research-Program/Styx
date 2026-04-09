7844f3e58e55a6f08ba4e3e63bd788f2e0ed67e8
supergoodname77 <epascuales@outlook.com>
2026-04-09 17:36:47 -0500

    lobotomy and evil

--- machines/pi-5/jupiter-fsw/jupiter.nix
+++ machines/pi-5/jupiter-fsw/jupiter.nix
@@ -1,30 +1,49 @@
-{ rustPlatform, fetchFromGitHub, pkg-config, libudev-zero, systemd, libgpiod, src, basler-pylon }:
+{ pkgs, src, basler-pylon }:
 
-rustPlatform.buildRustPackage (finalAttrs: {
+let
+  isolatedSrc = pkgs.runCommand "isolated-styx-src" {} ''
+    cp -r ${src} $out
+    chmod -R +w $out
+    
+    rm -f $out/Cargo.toml
+    rm -f $out/Cargo.lock
+  '';
+in
+pkgs.rustPlatform.buildRustPackage {
   pname = "jupiter-fsw";
   version = "0.1.1";
+  
+  # lobotomized source
+  src = isolatedSrc;
 
-  nativeBuildInputs = [ pkg-config libudev-zero ];
+  sourceRoot = "isolated-styx-src/machines/pi-5/jupiter-fsw";
 
-  buildInputs = [ systemd libgpiod ];
+  cargoHash = ""; 
 
-  inherit src;
+  nativeBuildInputs = [ 
+    pkgs.pkg-config 
+    pkgs.makeWrapper
+  ];
+
+  buildInputs = [ 
+    pkgs.systemd 
+    pkgs.libgpiod 
+    pkgs.libusb1
+    pkgs.zlib
+    basler-pylon 
+  ];
 
   PYLON_ROOT = "${basler-pylon}/opt/pylon";
-  buildAndTestSubdir = "machines/pi-5/jupiter-fsw";
 
   postInstall = ''
-    wrapProgram $out/bin/odin-compute \
-      --set GENICAM_GENTL64_PATH "${basler-pylon}/opt/pylon/lib/gentlproducer/gtl"
+    wrapProgram $out/bin/jupiter-fsw \
+      --set GENICAM_GENTL64_PATH "${basler-pylon}/opt/pylon/lib/gentlproducer/gtl" \
+      --set PYLON_GENTL64_PATH "${basler-pylon}/opt/pylon/lib/gentlproducer/gtl" \
+      --set LD_LIBRARY_PATH "${basler-pylon}/opt/pylon/lib:${pkgs.lib.makeLibraryPath [ pkgs.libusb1 pkgs.zlib pkgs.stdenv.cc.cc.lib ]}"
   '';
 
-  buildFeatures = [ "packet_logging" ];
-
-  # cargoHash = "sha256-7gA7qllYS66LPlVnqMBoRFZVAXlfis32jsxCevO2Ob0=";
-  # cargoLock = {
-  #   lockFile = ./Cargo.lock;
-  # };
-  cargoHash = "sha256-Pnx1Fyf/ABLru36AOoIy+BQjvIbkmanBIacvtxuCJVQ=";
+  # Disable packet_logging feature if it's currently causing issues, or add it back:
+  # buildFeatures = [ "packet_logging" ];
 
   doCheck = false;
-})
+}
