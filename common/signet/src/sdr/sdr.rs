6a8744fdf8228ed411f145dd22a34f2f0c5675dd
supergoodname77 <epascuales@outlook.com>
2026-04-10 18:47:23 -0500

    Downsampling

--- common/signet/src/sdr/sdr.rs
+++ common/signet/src/sdr/sdr.rs
@@ -6,19 +6,91 @@ use crate::sdr::radio_config::{
     BUFF_SIZE,
     TARGET_PACKET_SIZE};
 use bincode::de::read;
-// use rustfft::num_complex::Complex;
-use num_complex::Complex;
+use rustfft::num_complex::Complex;
 use soapysdr::{Device, Direction, RxStream};
-use std::time::{SystemTime, UNIX_EPOCH};
-
-use sdr::fir::FIR;
+use std::{any::Any, time::{SystemTime, UNIX_EPOCH}};
 use crate::error::SignalError;
+use scirs2_signal::{
+    filter::firwin,
+    parallel_filtering_v2::{parallel_fir_filter, ParallelFIRConfig, StreamingFIRFilter},
+    resampling::downsample,
+};
+
+struct Downsampler {
+    filter_i: StreamingFIRFilter,
+    filter_q: StreamingFIRFilter,
+    decimation_factor: usize,
+    i_buf: Vec<f64>,
+    q_buf: Vec<f64>,
+}
+impl Downsampler {
+
+    pub fn new(input_sample_rate: f32, decimation_factor: usize, target_cutoff_hz: f32) -> Self {
+        let n_taps = 818;
+        let input_nyquist = input_sample_rate / 2.0;
+        let normalized_cutoff = (target_cutoff_hz / input_nyquist) as f64;
+        let window = "blackman";
+        
+        let taps_i = firwin(n_taps, normalized_cutoff, window, true).unwrap();
+        let taps_q = firwin(n_taps, normalized_cutoff, window, true).unwrap();
+
+        Self {
+            filter_i: StreamingFIRFilter::new(taps_i).unwrap(),
+            filter_q: StreamingFIRFilter::new(taps_q).unwrap(),
+            decimation_factor,
+            i_buf: vec![0.0; READ_CHUNK_SIZE],
+            q_buf: vec![0.0; READ_CHUNK_SIZE],
+        }
+    }
+
+    fn downsample(&mut self, raw_samples: &[Complex<f32>]) -> Vec<Complex<f32>>{
+        let sample_len = raw_samples.len();
+
+        for i in 0..sample_len {
+            self.i_buf[i] = raw_samples[i].re as f64;
+            self.q_buf[i] = raw_samples[i].im as f64;
+        }
+
+        let i_filtered = self.filter_i.process_block(&self.i_buf);
+        let q_filtered = self.filter_q.process_block(&self.q_buf);
+
+        // Return downsampled i and q components zipped up into complex type for future manipulation
+        // with Rust fft
+        i_filtered.into_iter()
+                .zip(q_filtered.into_iter())
+                .step_by(self.decimation_factor)
+                .map(|(i,q,)| Complex::new(i as f32, q as f32))
+                .collect()
+    }
+}
+
+impl Default for Downsampler {
+    fn default() -> Self {
+        // Ideal N-taps according to Fred Harris rule of thumb and Kaiser windowing formula
+        let n_taps = 818;
+        let cutoff = 45_000.0 / 1_500_000.0;
+        let window  = "blackman";
+        
+        let taps_i = firwin(n_taps, cutoff, window, true).unwrap();
+        let taps_q = firwin(n_taps, cutoff, window, true).unwrap();
+
+        Self {
+            filter_i: StreamingFIRFilter::new(taps_i).unwrap(),
+            filter_q:  StreamingFIRFilter::new(taps_q).unwrap(),
+            decimation_factor: 30,
+            i_buf: vec![0.0; READ_CHUNK_SIZE],
+            q_buf: vec![0.0; READ_CHUNK_SIZE],
+        }
+    }
+}
+
+
+
 pub struct SDR {
     device: Device,
     stream: RxStream<Complex<f32>>,
     read_buffer: [Complex<f32>; READ_CHUNK_SIZE],
-    downsampler: FIR<Complex<f32>>,
-
+    downsampler: Downsampler,
 }
 
 use log::{error, info, LevelFilter};
@@ -50,9 +122,10 @@ impl SDR {
             device,
             stream,
             read_buffer: [Complex::new(0.0, 0.0); READ_CHUNK_SIZE],
-            FIR::new()
+            downsampler: Downsampler::default(),
         })
     }
+
 
     pub fn read_and_timestamp(&mut self, slice: &mut [Complex<f32>; BUFF_SIZE]) -> Result<(u128, usize), SignalError> {
         let mut time_stamp = None;
@@ -63,8 +136,12 @@ impl SDR {
                 .stream
                 .read(&mut [&mut self.read_buffer], 100_000)
                 .map_err(|e| SignalError::StreamReadError(head))?;
-            let end = head + read_len;
 
+            let downsampled_signal = self.downsampler.downsample(&self.read_buffer[..read_len]);
+            let down_chunk_size = downsampled_signal.len();
+            let end = head + down_chunk_size;
+
+            
             if time_stamp.is_none() {
                 time_stamp = Some(
                     SystemTime::now()
@@ -76,9 +153,18 @@ impl SDR {
 
             let max: usize = slice.len();
             if end <= max {
-                slice[head..end].copy_from_slice(&self.read_buffer[..read_len]);
+                slice[head..end].copy_from_slice(&downsampled_signal);
                 head = end;
-            } 
+            } else {
+
+                // May want to introduce overflow buffer of stuff that can't fit into packet size and store it in sdr
+                // to be used in next call
+                // Handle buffer overrun if a chunk pushes head past BUFF_SIZE
+                let space_left = max - head;
+                slice[head..max].copy_from_slice(&downsampled_signal[..space_left]);
+                head = max;
+                break;
+            }
 
             // self.accumulator[prev_head..head] = self.read_buffer[..read_len];
             // accumulator.extend_from_slice(&self.buffer[..len]);
