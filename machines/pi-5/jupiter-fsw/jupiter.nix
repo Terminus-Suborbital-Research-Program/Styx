2c7a27ae6629542efc8a4f5b40fe0d45392c9cb2
Ethan Pascuales <epascuales@outlook.com>
2026-04-20 18:35:40 -0500

    Basler nix (#147)
    * hash update
    
    * Remove icarus, make hal and monotonics versions consistent, fix build errors
    
    * An attempt at sanity
    
    * Monorepo slaying
    
    * Trying to block upward travesal
    
    * No hash
    
    * Empty hash to find hash
    
    * d
    
    * hash
    
    * lobotomy and evil
    
    * k
    
    * hash
    
    * merged
    
    * try fetch from github workaround
    
    * minor fix
    
    * Hash bump
    
    * Revert to src
    
    * exclude
    
    * Jupiter workspace
    
    * Try using lock
    
    * semicolon
    
    * This synax
    
    * set
    
    * set
    
    * fix signet
    
    * Try w cargo make
    
    * no iso src
    
    * source root fix
    
    * try w hash
    
    * hash
    
    * s
    
    * lib add
    
    * still no sub?
    
    * Try direct from git
    
    * like  this?
    
    * k
    
    * hash
    
    * Try with v4l deps
    
    * Offerings to please the great sandbox
    
    * no audit
    
    * Prevent sigkill?
    
    * Still sigkilling so likely oom
    
    * Try with flags explicitly in buildphase
    
    * line error
    
    * Try with make for only core packages
    
    * works on my system
    
    * Try abandoning cargo make
    
    * try refresh hash
    
    * reversion and no lc
    
    * k
    
    * k
    
    * Hash
    
    * k
    
    * hash bump
    
    * evil
    
    * clean up a bit

--- machines/pi-5/jupiter-fsw/jupiter.nix
+++ machines/pi-5/jupiter-fsw/jupiter.nix
@@ -1,26 +1,89 @@
-{ rustPlatform, fetchFromGitHub, pkg-config, libudev-zero, systemd, libgpiod, src, basler-pylon }:
-
-rustPlatform.buildRustPackage (finalAttrs: {
+{ pkgs, src, lib,fetchFromGitHub ,basler-pylon }:
+let
+  soapyextra = pkgs.soapysdr.override {
+    extraPackages = [ 
+      pkgs.soapyairspy 
+      pkgs.soapyrtlsdr 
+    ];
+  };
+in
+pkgs.rustPlatform.buildRustPackage {
   pname = "jupiter-fsw";
   version = "0.1.1";
+  
+  src = fetchFromGitHub {
+    owner = "Terminus-Suborbital-Research-Program";
+    repo = "Styx";
+    rev = "refs/heads/Basler-Nix";
+    fetchSubmodules = true;
+    hash = "sha256-IFCojXWRR638uhgaj/TYxmAcR/3+rw/MtxhBzwCK1YM=";
+  };
 
-  nativeBuildInputs = [ pkg-config libudev-zero ];
+  nativeBuildInputs = [ 
+    pkgs.pkg-config 
+    pkgs.makeWrapper
+    pkgs.cargo-make
+    pkgs.rustPlatform.bindgenHook
+  ];
 
-  buildInputs = [ systemd libgpiod ];
+  buildInputs = [ 
+    pkgs.systemd 
+    pkgs.libgpiod 
+    pkgs.libusb1
+    pkgs.zlib
+    pkgs.clang              
+    pkgs.llvmPackages.libclang 
+    pkgs.linuxHeaders
+    basler-pylon 
+    soapyextra        
+    pkgs.airspy
+  ];
 
-  inherit src;
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+  BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include";
+  SOAPY_SDR_PLUGIN_PATH = "${soapyextra}/lib/SoapySDR/modules0.8";
+
+  CARGO_PROFILE_RELEASE_LTO = "false";
+  CARGO_PROFILE_RELEASE_CODEGEN_UNITS = "16";
+
+  cargoBuildFlags = [ "-j" "2" ];
+
+  
+  # cargo make --profile release build-host -- --jobs 2 --target ${pkgs.stdenv.hostPlatform.rust.rustcTarget}  
+
+  buildPhase = ''
+    runHook preBuild
+
+    sed -i 's/HOST_PACKAGES = "-p jupiter-fsw -p odin-compute -p munin"/HOST_PACKAGES = "-p jupiter-fsw -p odin-compute -p munin --release --jobs 2 --target ${pkgs.stdenv.hostPlatform.rust.rustcTarget}"/g' Makefile.toml
+    
+    export RUSTFLAGS="-C lto=off -C codegen-units=16 $RUSTFLAGS"
+    cargo make build-host
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    
+    mkdir -p $out/bin
+    cp target/${pkgs.stdenv.hostPlatform.rust.rustcTarget}/release/jupiter-fsw $out/bin/ 2>/dev/null || cp machines/pi-5/jupiter-fsw/target/${pkgs.stdenv.hostPlatform.rust.rustcTarget}/release/jupiter-fsw $out/bin/    
+    runHook postInstall
+  '';
 
   PYLON_ROOT = "${basler-pylon}/opt/pylon";
-  buildAndTestSubdir = "machines/pi-5/jupiter-fsw";
 
   postInstall = ''
-    wrapProgram $out/bin/odin-compute \
-      --set GENICAM_GENTL64_PATH "${basler-pylon}/opt/pylon/lib/gentlproducer/gtl"
+    wrapProgram $out/bin/jupiter-fsw \
+      --set GENICAM_GENTL64_PATH "${basler-pylon}/opt/pylon/lib/gentlproducer/gtl" \
+      --set PYLON_GENTL64_PATH "${basler-pylon}/opt/pylon/lib/gentlproducer/gtl" \
+      --set LD_LIBRARY_PATH "${basler-pylon}/opt/pylon/lib:${pkgs.lib.makeLibraryPath [ pkgs.libusb1 pkgs.zlib pkgs.stdenv.cc.cc.lib ]}"
   '';
 
   buildFeatures = [ "packet_logging" ];
+  # cargoLock = {
+    # lockFile = ./Cargo.lock;
+  # };
 
-  cargoHash = "sha256-M3vbkixpirKhxSIiEGIhqGe7+VsEFunzREzbD4yHPrk=";
-
+  cargoHash = "sha256-QBiPk6Yh8b+jWLLYkvhkO+RoikiwaUHe9MaMCF/ysrY=";
   doCheck = false;
-})
+  auditable = false;
+}
